0f0f7d3f4f989b0488633530c785c2bb57fa72fc
Crowdbotics <team@crowdbotics.com>
2020-11-06 07:37:46 +0000

    remove package blueprint-react-native-settings-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -6,7 +6,6 @@ import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import SignIn11168484Navigator from '../features/SignIn11168484/navigator';
-import Settings2168483Navigator from '../features/Settings2168483/navigator';
 
 /**
  * new navigators can be imported here
@@ -16,7 +15,6 @@ const AppNavigator = {
 
     //@BlueprintNavigationInsertion
 SignIn11168484: { screen: SignIn11168484Navigator },
-Settings2168483: { screen: Settings2168483Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
